eddb19967cae40bb586fb32c36d7284f2d649bce
annoy21 <69495151+annoy21@users.noreply.github.com>
2021-04-27 22:19:55 +0530

    my-first-commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    var typed = new Typed(".typing", {
+        strings: ["Student","Web-developer", "Blogger"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Student","Web-developer", "Blogger"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    
